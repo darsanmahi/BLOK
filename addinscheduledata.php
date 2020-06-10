5d2591a64a6d97379790b36ed8e7000df256cac4
Priyadarsan <darsanprapanjan@gmail.com>
2020-06-10 15:31:26 +0530

    Alter Schedule

---
+++ addinscheduledata.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="node_modules/bootstrap-social/bootstrap-social.css">
+    <meta name="viewport" content="width=device-width, initial scale=1,shrink-to-fit=no">
+    <style>
+        body{
+            background-image: url(./images/roman-bozhko-PypjzKTUqLo-unsplash.jpg);
+            color: black;
+            background-repeat: no-repeat;
+            padding: 270px 0px 0px 0px;
+            background-size: cover;
+            text-align: left;
+            font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif
+        }
+        .navbar-dark{
+            background-color: black;
+        }
+    </style>
+    <body>
+        <nav class="navbar navbar-dark navbar-expand-sm fixed-top">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Navbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand mr-auto" href="#"><img src="./images/3be847d8-395b-4d3b-a493-c75ae865e1b5_200x200.png" height="45" width="42"></a>
+                    <div class="collapse navbar-collapse" id="Navbar">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-home"></i> Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="displayrecords.php"><i class="fa fa-book"></i> View Bookings</a></li>
+                            <li class="nav-item"><a class="nav-link" href="adduser.php"><i class="fa fa-user-circle-o"></i> Add User</a></li>
+                            <li class="nav-item"><a class="nav-link" href="addsecretary.php"><i class="fa fa-user-circle"></i> Add Secretary</a></li>
+                            <li class="nav-item"><a class="nav-link" href="bookadmin.php"><i class="fa fa-book"></i> Book Hall</a></li>
+                            <li class="nav-item active"><a class="nav-link" href="alterschedule.php"><i class="fa fa-building-o"></i> Alter Schedule</a></li>
+                        </ul>
+                        <span>
+                            <a href="logout.php"><button type="button" class="btn btn-light btn-md">
+                            <i class="fa fa-sign-out"></i> Log Out
+                            </button></a>
+                        </span>
+                    </div>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="row row-content">
+                <div class="col-12 col-md-6">
+        <?php
+            if(isset($_POST['add_btn'])){
+                $blk=$_POST['Block'];
+                $db=mysqli_connect("localhost","root","",$blk);
+                $hno=$_POST['hno'];
+                $day=$_POST['day'];
+                $pno=$_POST['pno'];
+                $query="INSERT INTO {$day}(hallnumber,startingperiod) values ('$hno','$pno')";
+                $result=mysqli_query($db,$query);
+                if($result){
+                    echo '<h2>Schedule Altered Successfully</h2>';
+                    ?>
+                    </div>
+                    </div>
+                    <div class="row row-content">
+                    <a href="alterschedule.php" type="button" class="btn btn-primary offset-2"><i class="fa fa-chevron-left"></i> Back </a>
+                    </div>
+                    <?php
+                }
+            }
+            elseif(isset($_POST['remove_btn'])){
+                $blk=$_POST['Block'];
+                $db=mysqli_connect("localhost","root","",$blk);
+                $hno=$_POST['hno'];
+                $day=$_POST['day'];
+                $pno=$_POST['pno'];
+                $q1="SELECT * FROM {$day} where hallnumber='$hno' and startingperiod='$pno'";
+                $r1=mysqli_query($db,$q1);
+                $rc=mysqli_num_rows($r1);
+                if($rc!=0){
+                    $query="DELETE from {$day} where hallnumber='$hno' and startingperiod='$pno'";
+                    $result=mysqli_query($db,$query);
+                    if($result){
+                        echo '<h2>Schedule Altered Successfully</h2>';
+                        ?>
+                        </div>
+                        </div>
+                        <div class="row row-content">
+                        <a href="alterschedule.php" type="button" class="btn btn-primary offset-2"><i class="fa fa-chevron-left"></i> Back </a>
+                        </div>
+                        <?php
+                    }
+                }
+                elseif($rc==0){
+                    echo '<h2>Invalid Input</h2>';
+                    ?>
+                        </div>
+                        </div>
+                        <div class="row row-content">
+                        <a href="alterschedule.php" type="button" class="btn btn-primary ml-5"><i class="fa fa-chevron-left"></i> Back </a>
+                        </div>
+                        <?php
+                }
+            }
+        ?>
+        </div>
+    </body>
+</html>
--- alterschedule.php
+++ alterschedule.php
@@ -1,28 +1,23 @@
 <!DOCTYPE html>
 <html>
-<meta name="viewport" content="width=device-width, initial scale=1,shrink-to-fit=no">
-<link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="node_modules/bootstrap-social/bootstrap-social.css">
-
+    <meta name="viewport" content="width=device-width, initial scale=1,shrink-to-fit=no">
     <style>
-        @import url("https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css");
         body{
-            background-image: url(./images/sharon-mccutcheon-eMP4sYPJ9x0-unsplash.jpg);
+            background-image: url(./images/roman-bozhko-PypjzKTUqLo-unsplash.jpg);
+            color: black;
             background-repeat: no-repeat;
+            padding: 200px 0px 0px 0px;
             background-size: cover;
-            font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-            padding: 100px 0px 0px 0px;
-        }
-        .logo{
-            float: right;
+            text-align: left;
+            font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif
         }
         .navbar-dark{
-            background-color: #191919;
+            background-color: black;
         }
-        </style>
-    <head>
-        <title>Hall Booking Page</title>
-    </head>
+    </style>
     <body>
         <nav class="navbar navbar-dark navbar-expand-sm fixed-top">
             <div class="container">
@@ -32,12 +27,12 @@
                 <a class="navbar-brand mr-auto" href="#"><img src="./images/3be847d8-395b-4d3b-a493-c75ae865e1b5_200x200.png" height="45" width="42"></a>
                     <div class="collapse navbar-collapse" id="Navbar">
                         <ul class="navbar-nav mr-auto">
-                            <li class="nav-item"><a class="nav-link" href="hallissuedisp.php"><i class="fa fa-home"></i> Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-home"></i> Home</a></li>
                             <li class="nav-item"><a class="nav-link" href="displayrecords.php"><i class="fa fa-book"></i> View Bookings</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-user-circle-o"></i> Add User</a></li>
+                            <li class="nav-item"><a class="nav-link" href="adduser.php"><i class="fa fa-user-circle-o"></i> Add User</a></li>
                             <li class="nav-item"><a class="nav-link" href="addsecretary.php"><i class="fa fa-user-circle"></i> Add Secretary</a></li>
-                            <li class="nav-item active"><a class="nav-link" href="#"><i class="fa fa-book"></i> Book Hall</a></li>
-                            <li class="nav-item"><a class="nav-link" href="alterschedule.php"><i class="fa fa-building-o"></i> Alter Schedule</a></li>
+                            <li class="nav-item"><a class="nav-link" href="bookadmin.php"><i class="fa fa-book"></i> Book Hall</a></li>
+                            <li class="nav-item active"><a class="nav-link" href="#"><i class="fa fa-building-o"></i> Alter Schedule</a></li>
                         </ul>
                         <span>
                             <a href="logout.php"><button type="button" class="btn btn-light btn-md">
@@ -50,9 +45,9 @@
         <div class="container">
             <div class="row row-content">
                 <div class="col-12 col-md-9">
-                    <form action="bookadmin1.php" method="POST">
-                        <div class="col-12 offset-2 align-contents-center">
-                            <h2>Book New Hall</h2><br>
+                    <form action="addinscheduledata.php" method="POST">
+                        <div class="col-12 align-contents-center">
+                            <h2>Alter Schedule</h2><br>
                             <div class="form-group row">
                                 <label for="Block" class="col-form-label col-12 col-md-2">Block</label>
                                 <div class="col-md-3">
@@ -65,39 +60,36 @@
                                 </div>
                             </div>
                             <div class="form-group row">
+                                <label for='hallnumber' class="col-12 col-md-2 col-form-label"> Hall Number</label>
+                                <div class="col-md-3">
+                                    <input type="text" class="form-control" id="hallnumber" name="hno" placeholder="Hall Number" required>
+                                </div>
+                            </div>
+                            <div class="form-group row">
                                 <label for='period' class="col-12 col-md-2 col-form-label">Period Number</label>
                                 <div class="col-md-3">
                                     <input type="number" min='1' max='9' class="form-control" id="period" name="pno" placeholder="Period Number" required>
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for='date' class="col-12 col-md-2 col-form-label">Date</label>
+                                <label for='day' class="col-12 col-md-2 col-form-label">Day</label>
                                 <div class="col-md-3">
-                                    <input type="date" class="form-control" id="date" name="date" required>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for='classcode' class="col-12 col-md-2 col-form-label">Class Code</label>
-                                <div class="col-md-3">
-                                    <input type="number" id="classcode" class="form-control" name="classcode" placeholder="Class Code" required>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for='sname' class="col-12 col-md-2 col-form-label">Staff Name</label>
-                                <div class="col-md-3">
-                                    <input type="text" name="sname" class="form-control" id="sname" placeholder="Staff Name" required>
+                                    <input type="day" class="form-control" id="day" placeholder="Day" name="day" required>
                                 </div>
                             </div>
                             <div class="col-12 offset-1 col-sm-4 align-contents-center">
-                                <button class="btn btn-primary" type="submit" name="Enter">Check the Availability</button>
+                                <div class="btn-group">
+                                    <button class="btn btn-primary" type="submit" name="add_btn">Add</button>
+                                    <button class="btn btn-warning" type="submit" name="remove_btn">Remove</button>
+                                </div>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
+        <script src="node_modules/jquery/dist/jquery.min.js"></script>
+        <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+        <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     </body>
-    <script src="node_modules/jquery/dist/jquery.min.js"></script>
-    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
-    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </html>
